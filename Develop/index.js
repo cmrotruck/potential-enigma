27fc127addee8106f0097e916b99ce97bb13cd9c
cmrotruck <crotruck@gmail.com>
2021-12-24 21:31:16 -0500

    added user questions to user array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'Enter is the project title: ',
+    'Enter the project description: ',
+    'Enter the project installation instructions: ',
+    'Enter the usage information: ',
+    'Enter the contribution guidelines: ',
+    'Enter the test instructions: ',
+    'Choose a license:',
+    'Enter your GitHub username: ',
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
